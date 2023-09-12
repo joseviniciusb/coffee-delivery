64e41f2777fa6438cb98c6b0e3a5f0b0f77c1213
joseviniciusb <josevbluz@outlook.com>
2023-09-12 14:16:58 -0300

    feat: componentizar intro e adicionar layout padrao

---
+++ src/styles/layouts/DefaultLayout/index.tsx
@@ -0,0 +1,11 @@
+
+import { Header } from "../../../components/Header";
+import { LayoutContainer } from "./styles";
+
+export const DefautLayout = () => {
+  return (
+    <LayoutContainer>
+      <Header />
+    </LayoutContainer>
+  );
+};
