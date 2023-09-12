bbfeceae5bc41b13eae6c47e30c6869003835875
joseviniciusb <josevbluz@outlook.com>
2023-09-12 14:16:11 -0300

    style: estilizar intro

--- src/styles.ts
+++ src/styles.ts
@@ -1,7 +1,56 @@
 import styled from "styled-components";
 
-export const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: #bf4f74;
+export const IntroContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  max-width: 90rem;
+  
+
+  section {
+    display: flex;
+    flex-direction: column;
+    gap: 6rem;
+  }
+`;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  width: 36rem;
+
+  h1 {
+    font-family: "Baloo 2", cursive;
+    color: ${(props) => props.theme["gray-700"]};
+    font-size: 2rem;
+  }
+
+  p {
+    font-family: "Roboto";
+    font-size: 1.2rem;
+    color: ${(props) => props.theme["base-subtitle"]};
+  }
+`;
+
+export const ItemsContainer = styled.div`
+  height: auto;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  font-family: "Roboto";
+  color: ${(props) => props.theme["gray-500"]};
+  font-size: 16px;
+
+  div {
+    display: flex;
+    min-width: 15rem;
+    align-items: center;
+    gap: 0.5rem;
+  }
+`;
+
+export const ImageContainer = styled.div`
+  img {
+    width: 30rem;
+  }
 `;
