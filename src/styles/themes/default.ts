4c2098637feaa67a28f9e85c1d246540a9792c91
joseviniciusb <josevbluz@outlook.com>
2023-09-13 18:49:08 -0300

    style: ajustar cores tematicas pro padrão do projeto

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,32 +1,21 @@
 export const defaultTheme = {
-  "white": "#FFFFFF",
-  "base-card": "#F3F2F2",
-
+   "base-title": "#272221",
   "base-subtitle": "#403937",
+  "base-text": "#574F4D",
+  "base-label": "#8D8686",
+  "base-hover": "#D7D5D5",
+  "base-button": "#E6E5E5",
+  "base-input": "#EDEDED",
+  "base-card": "#F3F2F2",
+  "background": "#FAFAFA",
+  "white": "#FFFFFF",
 
+  "purple": "#8047F8",
   "purple-light": "#EBE5F9",
   "purple-dark": "#4B2995",
-
+  "yellow": "#DBAC2C",
   "yellow-light": "#F1E9C9",
   "yellow-dark": "#C47F17",
-
-  "gray-100": "#E1E1E6",
-  "gray-300": "#C4C4CC",
-  "gray-400": "#8D8D99",
-  "gray-500": "#7C7C8A",
-  "gray-600": "#323238",
-  "gray-700": "#29292E",
-  "gray-800": "#202024",
-  "gray-900": "#121214",
-
-  "green-300": "#00B37E",
-  "green-500": "#00875F",
-  "green-700": "#015F43",
-
-  "red-500": "#AB222E",
-  "red-700": "#7A1921",
-
-  "yellow-500": "#FBA94C",
 
   "brand-purple": "#8047F8",
 };
