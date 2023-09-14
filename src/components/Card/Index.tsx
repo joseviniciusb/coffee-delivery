dade03d697674e107ad29b2a0395cfdcbe169784
joseviniciusb <josevbluz@outlook.com>
2023-09-14 16:47:47 -0300

    feat: implementar contador de itens pra adicionar no carrinho

--- src/components/Card/Index.tsx
+++ src/components/Card/Index.tsx
@@ -1,154 +1,166 @@
 import { ShoppingCart } from "phosphor-react";
-import { CardContainer, CoffeListContainer, CounterContainer } from "../../pages/Home/styles";
+import {
+  CardContainer,
+  CoffeListContainer,
+  CounterContainer,
+} from "../../pages/Home/styles";
 
-import TradicionalExpressoIcon from "../../assets/coffes/TradicionalExpressoIcon.svg"
+import TradicionalExpressoIcon from "../../assets/coffes/TradicionalExpressoIcon.svg";
+import { useState } from "react";
 
 export const Card = () => {
+  const coffes = [
+    {
+      image: TradicionalExpressoIcon,
+      tag: "Tradicional",
+      name: "Café Tradicional",
+      description: "O tradicional café feito com água quente e grãos moídos",
+      price: 9.9,
+    },
+    {
+      image: TradicionalExpressoIcon,
+      tag: "Tradicional",
+      name: "Café Tradicional",
+      description: "O tradicional café feito com água quente e grãos moídos",
+      price: 9.9,
+    },
+    {
+      image: TradicionalExpressoIcon,
+      tag: "Tradicional",
+      name: "Café Tradicional",
+      description: "O tradicional café feito com água quente e grãos moídos",
+      price: 9.9,
+    },
+    {
+      image: TradicionalExpressoIcon,
+      tag: "Tradicional",
+      name: "Café Tradicional",
+      description: "O tradicional café feito com água quente e grãos moídos",
+      price: 9.9,
+    },
+    {
+      image: TradicionalExpressoIcon,
+      tag: "Tradicional",
+      name: "Café Tradicional",
+      description: "O tradicional café feito com água quente e grãos moídos",
+      price: 9.9,
+    },
+    {
+      image: TradicionalExpressoIcon,
+      tag: "Tradicional",
+      name: "Café Tradicional",
+      description: "O tradicional café feito com água quente e grãos moídos",
+      price: 9.9,
+    },
+    {
+      image: TradicionalExpressoIcon,
+      tag: "Tradicional",
+      name: "Café Tradicional",
+      description: "O tradicional café feito com água quente e grãos moídos",
+      price: 9.9,
+    },
+    {
+      image: TradicionalExpressoIcon,
+      tag: "Tradicional",
+      name: "Café Tradicional",
+      description: "O tradicional café feito com água quente e grãos moídos",
+      price: 9.9,
+    },
+    {
+      image: TradicionalExpressoIcon,
+      tag: "Tradicional",
+      name: "Café Tradicional",
+      description: "O tradicional café feito com água quente e grãos moídos",
+      price: 9.9,
+    },
+    {
+      image: TradicionalExpressoIcon,
+      tag: "Tradicional",
+      name: "Café Tradicional",
+      description: "O tradicional café feito com água quente e grãos moídos",
+      price: 9.9,
+    },
+    {
+      image: TradicionalExpressoIcon,
+      tag: "Tradicional",
+      name: "Café Tradicional",
+      description: "O tradicional café feito com água quente e grãos moídos",
+      price: 9.9,
+    },
+    {
+      image: TradicionalExpressoIcon,
+      tag: "Tradicional",
+      name: "Café Tradicional",
+      description: "O tradicional café feito com água quente e grãos moídos",
+      price: 9.9,
+    },
+    {
+      image: TradicionalExpressoIcon,
+      tag: "Tradicional",
+      name: "Café Tradicional",
+      description: "O tradicional café feito com água quente e grãos moídos",
+      price: 9.9,
+    },
+    {
+      image: TradicionalExpressoIcon,
+      tag: "Tradicional",
+      name: "Café Tradicional",
+      description: "O tradicional café feito com água quente e grãos moídos",
+      price: 9.9,
+    },
+    {
+      image: TradicionalExpressoIcon,
+      tag: "Tradicional",
+      name: "Café Tradicional",
+      description: "O tradicional café feito com água quente e grãos moídos",
+      price: 9.9,
+    },
+    {
+      image: TradicionalExpressoIcon,
+      tag: "Tradicional",
+      name: "Café Tradicional",
+      description: "O tradicional café feito com água quente e grãos moídos",
+      price: 9.9,
+    },
+  ];
 
-    const coffes = [
-        {
-          image: TradicionalExpressoIcon,
-          tag: "Tradicional",
-          name: "Café Tradicional",
-          description: "O tradicional café feito com água quente e grãos moídos",
-          price: 9.9,
-        },
-        {
-          image: TradicionalExpressoIcon,
-          tag: "Tradicional",
-          name: "Café Tradicional",
-          description: "O tradicional café feito com água quente e grãos moídos",
-          price: 9.9,
-        },
-        {
-          image: TradicionalExpressoIcon,
-          tag: "Tradicional",
-          name: "Café Tradicional",
-          description: "O tradicional café feito com água quente e grãos moídos",
-          price: 9.9,
-        },
-        {
-          image: TradicionalExpressoIcon,
-          tag: "Tradicional",
-          name: "Café Tradicional",
-          description: "O tradicional café feito com água quente e grãos moídos",
-          price: 9.9,
-        },
-        {
-          image: TradicionalExpressoIcon,
-          tag: "Tradicional",
-          name: "Café Tradicional",
-          description: "O tradicional café feito com água quente e grãos moídos",
-          price: 9.9,
-        },
-        {
-          image: TradicionalExpressoIcon,
-          tag: "Tradicional",
-          name: "Café Tradicional",
-          description: "O tradicional café feito com água quente e grãos moídos",
-          price: 9.9,
-        },
-        {
-          image: TradicionalExpressoIcon,
-          tag: "Tradicional",
-          name: "Café Tradicional",
-          description: "O tradicional café feito com água quente e grãos moídos",
-          price: 9.9,
-        },
-        {
-          image: TradicionalExpressoIcon,
-          tag: "Tradicional",
-          name: "Café Tradicional",
-          description: "O tradicional café feito com água quente e grãos moídos",
-          price: 9.9,
-        },
-        {
-          image: TradicionalExpressoIcon,
-          tag: "Tradicional",
-          name: "Café Tradicional",
-          description: "O tradicional café feito com água quente e grãos moídos",
-          price: 9.9,
-        },
-        {
-          image: TradicionalExpressoIcon,
-          tag: "Tradicional",
-          name: "Café Tradicional",
-          description: "O tradicional café feito com água quente e grãos moídos",
-          price: 9.9,
-        },
-        {
-          image: TradicionalExpressoIcon,
-          tag: "Tradicional",
-          name: "Café Tradicional",
-          description: "O tradicional café feito com água quente e grãos moídos",
-          price: 9.9,
-        },
-        {
-          image: TradicionalExpressoIcon,
-          tag: "Tradicional",
-          name: "Café Tradicional",
-          description: "O tradicional café feito com água quente e grãos moídos",
-          price: 9.9,
-        },
-        {
-          image: TradicionalExpressoIcon,
-          tag: "Tradicional",
-          name: "Café Tradicional",
-          description: "O tradicional café feito com água quente e grãos moídos",
-          price: 9.9,
-        },
-        {
-          image: TradicionalExpressoIcon,
-          tag: "Tradicional",
-          name: "Café Tradicional",
-          description: "O tradicional café feito com água quente e grãos moídos",
-          price: 9.9,
-        },
-        {
-          image: TradicionalExpressoIcon,
-          tag: "Tradicional",
-          name: "Café Tradicional",
-          description: "O tradicional café feito com água quente e grãos moídos",
-          price: 9.9,
-        },
-        {
-          image: TradicionalExpressoIcon,
-          tag: "Tradicional",
-          name: "Café Tradicional",
-          description: "O tradicional café feito com água quente e grãos moídos",
-          price: 9.9,
-        },
-      ];
-    
+  const [counters, setCounters] = useState(Array(coffes.length).fill(0));
 
+  const handleCounter = (index: number, increment: number) => {
+    const newCounters = [...counters];
+    newCounters[index] += increment;
 
-    return (
-        <>
-         <h1>Nossos cafés</h1>
-<CoffeListContainer>
-  {coffes.map((card) => {
-    return (
-      <CardContainer>
-        <img src={card.image} />
-        <h3>{card.tag}</h3>
-        <h2>{card.name}</h2>
-        <span>{card.description}</span>
-        <div>
-          <p>R$ {card.price}</p>
-          <nav>
-            <CounterContainer>
-              <p>-</p>
-              <p>0</p>
-              <p>+</p>
-            </CounterContainer>
-            <ShoppingCart size={38} />
-          </nav>
-        </div>
-      </CardContainer>
-    );
-  })}
-</CoffeListContainer>
-        </>
-       
-    )
-}
+    setCounters(newCounters);
+  };
+
+  return (
+    <>
+      <>
+        <h1>Nossos cafés</h1>
+        <CoffeListContainer>
+          {coffes.map((card, index) => {
+            return (
+              <CardContainer key={index}>
+                <img src={card.image} />
+                <h3>{card.tag}</h3>
+                <h2>{card.name}</h2>
+                <span>{card.description}</span>
+                <div>
+                  <p>R$ {card.price}</p>
+                  <nav>
+                    <CounterContainer>
+                      <p onClick={() => handleCounter(index, -1)}>-</p>
+                      <p>{counters[index]}</p>
+                      <p onClick={() => handleCounter(index, 1)}>+</p>
+                    </CounterContainer>
+                    <ShoppingCart size={38} />
+                  </nav>
+                </div>
+              </CardContainer>
+            );
+          })}
+        </CoffeListContainer>
+      </>
+    </>
+  );
+};
