9fcf383c616917219960a2253ffa3878d3968949
joseviniciusb <josevbluz@outlook.com>
2023-09-19 20:25:34 -0300

    type: tipar a lista de todos cafés

---
+++ src/types/Coffee.ts
@@ -0,0 +1,10 @@
+export interface Coffee {
+    id: number;
+    image: string;
+    tag: string | { tag1: string; tag2: string };
+    name: string;
+    description: string;
+    price: number;
+  }
+
+  
