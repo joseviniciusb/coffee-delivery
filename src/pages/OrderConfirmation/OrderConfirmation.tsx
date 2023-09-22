c757c3416eab5dd698df879da81dc1f67fc7ec50
joseviniciusb <josevbluz@outlook.com>
2023-09-22 18:03:57 -0300

    style: ajustar estilos dos textos do orderconfirm

--- src/pages/OrderConfirmation/OrderConfirmation.tsx
+++ src/pages/OrderConfirmation/OrderConfirmation.tsx
@@ -1,68 +1,83 @@
 import { CurrencyDollar, MapPin, Timer } from "phosphor-react";
 
 import styled from "styled-components";
+import { useLocation } from "react-router-dom";
 
 import IllustrationDeliveryRider from "../../assets/IllustrationDeliveryRider.svg";
 
+const OrderConfirmationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+`;
+
+const OrderInfoContainer = styled.div`
+  background-color: ${(props) => props.theme["background"]};
+  display: flex;
+  flex-direction: column;
+  background-color: #fafafa;
+  padding: 3rem 2rem 3rem 2rem;
+  gap: 1.5rem;
+`;
+
+const BorderWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 1rem;
+  margin-bottom: 10rem;
+
+  gap: 2.5rem;
+  white-space: normal;
+
+  font-size: 16px;
+
+  background: linear-gradient(white, white) padding-box,
+    linear-gradient(
+        to right,
+        ${(props) => props.theme["yellow-dark"]},
+        ${(props) => props.theme["purple-dark"]}
+      )
+      border-box;
+
+  border: 2px solid transparent;
+  border-radius: 6px 36px 6px 36px;
+  overflow: hidden;
+`;
+
+const InfoTextContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+  max-width: 400px;
+
+  div :nth-child(2) {
+    font-weight: bold;
+  }
+`;
+
+const DeliveryRiderIlustrationContainer = styled.div``;
+
+const FlexContainer = styled.div`
+  display: flex;
+  gap: 3rem;
+`;
+
+const StyledTitle = styled.h1`
+  font-family: "Baloo 2";
+  font-size: 32px;
+  color: ${(props) => props.theme["yellow-dark"]};
+`;
+
+const StyledSubTitle = styled.p`
+  font-size: 20px;
+  color: ${(props) => props.theme["base-subtitle"]};
+`;
+
+const InfoText = styled.p``;
+
 export const OrderConfirmation = () => {
-  const OrderConfirmationContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 3rem;
-  `;
+  const location = useLocation();
 
-  const OrderInfoContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin-top: 1rem;
-    margin-bottom: 10rem;
-    padding: 2.5rem;
-    gap: 2.5rem;
-    white-space: normal;
-
-    font-size: 16px;
-
-    background: linear-gradient(white, white) padding-box,
-      linear-gradient(
-          to right,
-          ${(props) => props.theme["yellow-dark"]},
-          ${(props) => props.theme["purple-dark"]}
-        )
-        border-box;
-
-    border: 2px solid transparent;
-    border-radius: 6px 36px 6px 36px;
-  `;
-
-  const InfoTextContainer = styled.div`
-    display: flex;
-    gap: 1rem;
-    max-width: 400px;
-  `;
-
-  const DeliveryRiderIlustrationContainer = styled.div``;
-
-  const FlexContainer = styled.div`
-    display: flex;
-    gap: 3rem;
-  `;
-
-  const StyledTitle = styled.h1`
-    font-family: "Baloo 2";
-    font-size: 32px;
-    color: ${(props) => props.theme["yellow-dark"]};
-  `;
-
-  const StyledSubTitle = styled.p`
-    font-size: 20px;
-    color: ${(props) => props.theme["base-subtitle"]};
-  `;
-
-  const InfoText = styled.p`
-    :first-child {
-      font-weight: bold;
-    }
-  `;
+  console.log(location.state);
 
   return (
     <OrderConfirmationContainer>
@@ -74,29 +89,35 @@ export const OrderConfirmation = () => {
       </div>
 
       <FlexContainer>
-        <OrderInfoContainer>
-          <InfoTextContainer>
-            <MapPin size={32} weight="fill" color="purple" />
-            <InfoText>
-              Entrega em <span> Rua João Daniel Martinelli, 102</span> Farrapos
-              - Porto Alegre, RS
-            </InfoText>
-          </InfoTextContainer>
+        <BorderWrap>
+          <OrderInfoContainer>
+            <InfoTextContainer>
+              <MapPin size={32} weight="fill" color="purple" />
+              <div>
+                <InfoText>
+                  Entrega em <span> Rua João Daniel Martinelli, 102</span>
+                </InfoText>
+                <InfoText>Farrapos - Porto Alegre, RS</InfoText>
+              </div>
+            </InfoTextContainer>
 
-          <InfoTextContainer>
-            <Timer size={32} weight="fill" color="yellow" />
-            <InfoText>
-              Previsão de entrega <p>20 min - 30 min</p>
-            </InfoText>
-          </InfoTextContainer>
+            <InfoTextContainer>
+              <Timer size={32} weight="fill" color="yellow" />
+              <div>
+                <InfoText>Previsão de entrega</InfoText>
+                <p>20 min - 30 min</p>
+              </div>
+            </InfoTextContainer>
 
-          <InfoTextContainer>
-            <CurrencyDollar size={32} />
-            <InfoText>
-              Pagamento na entrega <p>Cartão de crédito</p>
-            </InfoText>
-          </InfoTextContainer>
-        </OrderInfoContainer>
+            <InfoTextContainer>
+              <CurrencyDollar size={32} />
+              <div>
+                <InfoText>Pagamento na entrega</InfoText>
+                <InfoText>Cartão de crédito</InfoText>
+              </div>
+            </InfoTextContainer>
+          </OrderInfoContainer>
+        </BorderWrap>
 
         <DeliveryRiderIlustrationContainer>
           <img src={IllustrationDeliveryRider}></img>
