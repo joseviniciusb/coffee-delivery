6c667bbe1aa3a6f0b402375da14b913255cae149
joseviniciusb <josevbluz@outlook.com>
2023-09-26 20:14:32 -0300

    feat: chamar API do openstreetmap pra localizar a cidade do usuario

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,9 +9,12 @@ import Location from "../../assets/Location.svg";
 import shoppingCart from "../../assets/shoppingCart.svg";
 import { useAppContext } from "../../contexts/ProductsContext";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 export const Header = () => {
   const { shoppingCartItems } = useAppContext();
+  const [city, setCity] = useState("");
+  const [stateAbbreviation, setStateAbbreviation] = useState("");
 
   const arrayTotalItems = shoppingCartItems.map((coffee) => coffee.count);
   const initialValue = 0;
@@ -20,6 +23,67 @@ export const Header = () => {
     (accumulator, currentValue) => accumulator + currentValue,
     initialValue
   );
+
+  const stateAbbreviations = {
+    Acre: "AC",
+    Alagoas: "AL",
+    Amapá: "AP",
+    Amazonas: "AM",
+    Bahia: "BA",
+    Ceará: "CE",
+    DistritoFederal: "DF",
+    EspíritoSanto: "ES",
+    Goiás: "GO",
+    Maranhão: "MA",
+    MatoGrosso: "MT",
+    MatoGrossoDoSul: "MS",
+    MinasGerais: "MG",
+    Pará: "PA",
+    Paraíba: "PB",
+    Paraná: "PR",
+    Pernambuco: "PE",
+    Piauí: "PI",
+    RioDeJaneiro: "RJ",
+    RioGrandeDoNorte: "RN",
+    RioGrandeDoSul: "RS",
+    Rondônia: "RO",
+    Roraima: "RR",
+    SantaCatarina: "SC",
+    SãoPaulo: "SP",
+    Sergipe: "SE",
+    Tocantins: "TO",
+  };
+
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      getCityInfo(position.coords.latitude, position.coords.longitude);
+    });
+  } else {
+    console.log("Not Available");
+  }
+
+  function getCityInfo(latitude, longitude) {
+    var geocodingUrl = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`;
+
+    fetch(geocodingUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data && data.address) {
+          var city = data.address.city;
+          var state = data.address.state;
+
+          var stateAbbreviation = stateAbbreviations[state] || state;
+
+          setCity(city);
+          setStateAbbreviation(stateAbbreviation);
+        } else {
+          console.log("City not found");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 
   return (
     <HeaderContainer>
@@ -30,7 +94,9 @@ export const Header = () => {
       <nav>
         <LocationContainer>
           <img src={Location}></img>
-          <span>Porto Alegre, RS</span>
+          <span>
+            {city}, {stateAbbreviation}
+          </span>
         </LocationContainer>
         <ShoppingCartIconContainer>
           <NavLink to="/checkout">
